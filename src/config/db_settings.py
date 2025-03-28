98e0c96f5a78843be666e1f0623f024eb9875eaf
artyombn <balabashinan@gmail.com>
2025-03-29 00:40:25 +0300

    Add base settings and async DB base model

---
+++ src/config/db_settings.py
@@ -0,0 +1,29 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    DOCKER: bool
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST_LOCAL: str
+    DB_HOST_DOCKER: str
+    DB_PORT: int
+    DB_NAME: str
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(BASE_DIR, ".env"),
+        extra="allow"
+    )
+
+    def get_db_url(self):
+        if self.DOCKER:
+            return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+                    f"{self.DB_HOST_DOCKER}:{self.DB_PORT}/{self.DB_NAME}")
+        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+                f"{self.DB_HOST_LOCAL}:{self.DB_PORT}/{self.DB_NAME}")
+
+settings = Settings()
---
+++ src/database/base_model.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+from config.db_settings import settings
+
+DATABASE_URL = settings.get_db_url()
+
+engine = create_async_engine(url=DATABASE_URL)
+async_session_maker = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr
+    def __tablename__(cls):
+        return f"{cls.__name__.lower()}s"
