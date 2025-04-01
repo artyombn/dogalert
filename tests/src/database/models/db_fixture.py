4f7de1ed5063e2a1f427cef515932055c6dc8bbd
artyombn <balabashinan@gmail.com>
2025-04-01 21:22:48 +0300

    Add db_fill_data fixture

--- tests/src/database/models/db_fixture.py
+++ tests/src/database/models/db_fixture.py
@@ -2,6 +2,9 @@ import pytest
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from src.database.models.base_model import Base
+from src.database.models.user import User
+from src.database.models.report import Report
+from src.database.models.pet import Pet
 
 DATABASE_URL = "sqlite+aiosqlite:///:memory:"
 
@@ -23,3 +26,26 @@ async def db_session():
         yield session
 
     await engine.dispose()
+
+@pytest.fixture(scope="function")
+async def db_fill_data(db_session: AsyncSession):
+    user = User(
+        telegram_id=1234567890,
+        username="artyombn",
+        first_name="Artem",
+    )
+    pet = Pet(name="Douglas")
+    report = Report(
+        title="Douglas lost",
+        content="Help to find Douglas!",
+        user=user,
+        pet=pet,
+    )
+
+    pet.owners.append(user)
+    pet.reports.append(report)
+
+    db_session.add_all([user, pet, report])
+    await db_session.commit()
+
+    return {"user": user, "pet": pet, "report": report}
