ffd6ca8717a086f13a19201ea796257a8ee0e196
artyombn <balabashinan@gmail.com>
2025-04-01 23:48:30 +0300

    Upd test create User

--- tests/src/database/models/test_user.py
+++ tests/src/database/models/test_user.py
@@ -1,12 +1,11 @@
 from sqlalchemy.future import select
 
-from src.database.models import User
+from src.database.models.user import User
 from .db_fixture import db_session, db_fill_data
+
 
 async def test_create_user(db_session, db_fill_data):
     user = db_fill_data["user"]
-    pet = db_fill_data["pet"]
-    report = db_fill_data["report"]
 
     stmt = select(User).where(User.username == "artyombn")
     result = await db_session.execute(stmt)
@@ -16,3 +15,6 @@ async def test_create_user(db_session, db_fill_data):
     assert is_user.telegram_id == user.telegram_id
     assert is_user.username == "artyombn"
     assert is_user.first_name == "Artem"
+    assert is_user.phone == None
+    assert is_user.pets == user.pets and is_user.pets[0].name == "Douglas"
+    assert is_user.reports == user.reports and is_user.reports[0].title == "Douglas lost"
