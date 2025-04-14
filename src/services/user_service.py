9c5febe6d54ed6c876e6670efcea5e050a76785e
artyombn <balabashinan@gmail.com>
2025-04-14 19:35:22 +0300

    User services start

---
+++ src/services/user_service.py
@@ -0,0 +1,12 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database.models.user import User as User_db
+
+
+class UserServices:
+    @classmethod
+    async def get_all_users(cls, session: AsyncSession):
+        query = select(User_db)
+        users = await session.execute(query)
+        return users.scalars().all()
