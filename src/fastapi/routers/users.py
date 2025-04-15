184dfa11038e8c7b8c56864ffa81434bb0d2a76c
artyombn <balabashinan@gmail.com>
2025-04-15 18:55:42 +0300

    upd: UserListResponse for Swagger UI

--- src/fastapi/routers/users.py
+++ src/fastapi/routers/users.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends
 from src.database.db_session import get_async_session
 from src.schemas.user import User as UserSchema
-from src.schemas.user import UserCreate
+from src.schemas.user import UserCreate, UserListResponse
 from src.services.user_service import UserServices
 
 router = APIRouter(
@@ -12,15 +12,21 @@ router = APIRouter(
 )
 
 
-@router.get("/", summary="Get all users", response_model=list[UserSchema])
-async def get_users_list(session: AsyncSession = Depends(get_async_session)) -> list[UserSchema]:
+@router.get("/", summary="Get all users", response_model=UserListResponse)
+async def get_users_list(session: AsyncSession = Depends(get_async_session)) -> UserListResponse:
     db_users = await UserServices.get_all_users(session)
-    return [UserSchema.model_validate(user) for user in db_users]
+    return UserListResponse(
+        total_users=len(db_users),
+        users=[UserSchema.model_validate(user) for user in db_users]
+    )
 
 @router.post("/create", summary="User creation", response_model=UserSchema)
 async def create_user(
         user_data: UserCreate,
+        telegram_id: int = 1,
         session: AsyncSession = Depends(get_async_session),
 ) -> UserSchema:
-    new_db_user = await UserServices.create_user(user_data, session)
+    new_db_user = await UserServices.create_user(user_data, session, telegram_id)
     return UserSchema.model_validate(new_db_user)
+
+
