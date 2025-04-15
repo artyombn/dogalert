93cb41e67f36ed1e329c30295171a787edbeee2f
artyombn <balabashinan@gmail.com>
2025-04-15 17:21:28 +0300

    add create user: service & router

--- src/fastapi/routers/users.py
+++ src/fastapi/routers/users.py
@@ -1,9 +1,9 @@
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from fastapi import APIRouter, Depends
 from src.database.db_session import get_async_session
 from src.schemas.user import User as UserSchema
+from src.schemas.user import UserCreate
 from src.services.user_service import UserServices
 
 router = APIRouter(
@@ -16,3 +16,11 @@ router = APIRouter(
 async def get_users_list(session: AsyncSession = Depends(get_async_session)) -> list[UserSchema]:
     db_users = await UserServices.get_all_users(session)
     return [UserSchema.model_validate(user) for user in db_users]
+
+@router.post("/create", summary="User creation", response_model=UserSchema)
+async def create_user(
+        user_data: UserCreate,
+        session: AsyncSession = Depends(get_async_session)
+):
+    new_db_user = await UserServices.create_user(user_data, session)
+    return UserSchema.model_validate(new_db_user)
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -4,6 +4,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.models.user import User as User_db
+from src.schemas.user import UserCreate
 
 
 class UserServices:
@@ -12,3 +13,11 @@ class UserServices:
         query = select(User_db)
         users = await session.execute(query)
         return users.scalars().all()
+
+    @classmethod
+    async def create_user(cls, user_data: UserCreate, session: AsyncSession) -> User_db:
+        db_user = User_db(**user_data.model_dump())
+        session.add(db_user)
+        await session.commit()
+        await session.refresh(db_user)
+        return db_user
