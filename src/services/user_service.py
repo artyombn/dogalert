f7a43fea3528a1b2e5646dfa35163f1d75973644
artyombn <balabashinan@gmail.com>
2025-04-17 15:19:33 +0300

    fix: add exceptions

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from src.database.models.user import User as User_db
-from src.schemas.user import UserCreate
+from src.schemas.user import UserCreate, UserUpdate
 
 
 logger = logging.getLogger(__name__)
@@ -26,12 +26,13 @@ class UserServices:
         return users.scalars().all()
 
     @classmethod
-    async def find_one_or_none_by_tgid(cls, telegram_id: int, session: AsyncSession) -> Sequence[User_db]:
-        query = (select(User_db).
-        where(User_db.telegram_id == telegram_id).
-        options(
-            selectinload(User_db.pets),
-            selectinload(User_db.reports),
+    async def find_one_or_none_by_tgid(cls, telegram_id: int, session: AsyncSession) -> User_db:
+        query = (
+            select(User_db).
+            filter_by(telegram_id=telegram_id).
+            options(
+                selectinload(User_db.pets),
+                selectinload(User_db.reports),
         )
         )
         user = await session.execute(query)
@@ -44,16 +45,31 @@ class UserServices:
             session: AsyncSession,
             telegram_id: int,
     ) -> User_db:
+        query = select(User_db).filter_by(telegram_id=telegram_id)
+        result = await session.execute(query)
+        user_exist = result.scalar_one_or_none()
+
+        if user_exist:
+            return None
+
         db_user = User_db(**user_data.model_dump(), telegram_id=telegram_id)
         session.add(db_user)
-        await session.commit()
-        await session.refresh(db_user)
+
+        try:
+            await session.commit()
+            await session.refresh(db_user)
+        except Exception as e:
+            await session.rollback()
+            raise Exception(f"Failed to create user: {str(e)}")
 
         # Eager load relations to avoid MissingGreenlet during serialization
         result = await session.execute(
-            select(User_db)
-            .options(selectinload(User_db.pets), selectinload(User_db.reports))
-            .where(User_db.id == db_user.id),
+            select(User_db).
+            filter_by(id=db_user.id).
+            options(selectinload(User_db.pets), selectinload(User_db.reports)),
         )
-        return result.scalar_one()
+        user = result.scalar_one()
+
+        return user
+
 
