291469534dd75aa0872b352e2af2726ecd0c8b86
artyombn <balabashinan@gmail.com>
2025-04-22 00:15:38 +0300

    add: PetReports model rebuild

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,6 +1,18 @@
-from .user import User, UserPet, UserReport, UserListResponse, UserPetsResponse, UserReportsResponse
-from .pet import Pet, PetPhoto, PetOwners, PetListResponse, PetOwnersResponse, PetReportsResponse, PetPhotosResponse
-from .report import Report, ReportUpdate, ReportCreate, ReportPhoto, ReportBase, ReportListResponse, ReportPhotoBase, ReportPhotoCreate, ReportPhotosResponse, ReportPhotoUpdate, ReportStatus
+from .user import (
+    User, UserPet, UserReport,
+    UserListResponse, UserPetsResponse, UserReportsResponse
+)
+
+from .pet import (
+    Pet, PetPhoto, PetOwners,
+    PetListResponse, PetOwnersResponse, PetReportsResponse, PetReports
+)
+
+from .report import (
+    Report, ReportUpdate, ReportCreate, ReportPhoto, ReportBase,
+    ReportListResponse, ReportPhotoBase, ReportPhotoCreate,
+    ReportPhotosResponse, ReportPhotoUpdate, ReportStatus
+)
 
 UserPet.model_rebuild()
 UserReport.model_rebuild()
@@ -8,4 +20,5 @@ UserPetsResponse.model_rebuild()
 UserReportsResponse.model_rebuild()
 PetOwners.model_rebuild()
 PetOwnersResponse.model_rebuild()
+PetReports.model_rebuild()
 PetReportsResponse.model_rebuild()
