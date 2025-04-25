d464ad227af0de90f46177fe6e149de3454dcc2a
artyombn <balabashinan@gmail.com>
2025-04-25 17:52:24 +0300

    add: separate data for fake pet creation

---
+++ src/database/management/pet_data.py
@@ -0,0 +1,84 @@
+class PetData:
+    pet_names: list
+    pet_breeds: list
+    pet_colors: list
+    pet_descriptions: list
+    pet_photos_links: list
+
+
+pet_data = PetData()
+
+pet_data.pet_names = [
+    "Барсик", "Мурка", "Рекс", "Боня", "Чарли", "Снежок", "Том", "Лаки", "Белка", "Стелла",
+    "Шарик", "Граф", "Пушок", "Жуля", "Тиша", "Луна", "Марс", "Рада", "Дикси", "Феликс",
+]
+
+pet_data.pet_breeds = [
+    "Мейн-кун", "Британская короткошерстная", "Сибирская", "Лабрадор", "Бульдог",
+    "Шотландская вислоухая", "Корги", "Доберман", "Чихуахуа", "Йоркширский терьер",
+    "Акита-ину", "Ротвейлер", "Персидская кошка", "Такса", "Боксер", "Шарпей", "Хаски",
+    "Сфинкс", "Русская голубая", "Далматин",
+]
+
+pet_data.pet_colors = [
+    "белый", "чёрный", "рыжий", "серый", "пятнистый", "коричневый", "золотистый", "шоколадный",
+    "песочный", "кремовый", "серо-белый", "чёрно-белый", "рыже-белый", "мраморный", "пепельный",
+    "бежевый", "тёмно-серый", "карамельный", "тёмно-коричневый", "триколор",
+]
+
+pet_data.pet_descriptions = [
+    "Очень ласковый и дружелюбный", "Обожает играть с мячиком", "Легко идёт на контакт",
+    "Боится громких звуков, но быстро привыкает", "Идеален для семьи с детьми",
+    "Тихий, аккуратный, хорошо воспитан", "Любит гулять на свежем воздухе",
+    "Обожает вкусняшки и мягкие пледы", "Очень умный и быстро обучается",
+    "Хорошо ладит с другими животными", "Настоящий охранник", "Нежный и спокойный",
+    "Немного стеснительный, но очень преданный", "Игривый и любознательный",
+    "Умеет выполнять команды", "С отличным аппетитом", "Обожает внимание",
+    "Требует немного терпения, но потом — лучший друг", "Любит сидеть на коленках",
+    "Прекрасный компаньон для прогулок",
+]
+
+pet_data.pet_photos_links = [
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/02/70/25/48/1000_F_270254841_otSDX3fPBgcbCiUdir82QjgyF8XSQwD5.jpg",
+  },
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/03/22/04/88/1000_F_322048853_DOj6xDy7Cpz98ZNi0I9UN708TZSqjt5m.jpg",
+  },
+  {
+    "url": "https://as2.ftcdn.net/v2/jpg/03/12/93/33/1000_F_312933371_vMqXBtR0s84b7WHGbUWpgIzVmhrgp8za.jpg",
+  },
+  {
+    "url": "https://as2.ftcdn.net/v2/jpg/00/77/67/35/1000_F_77673572_47MPcNB5sPB0DQ8lNvQ4KlxytOQsEWT9.jpg",
+  },
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/03/10/30/16/1000_F_310301633_QBQ3lSEcqhZnMUcAPbpLwaWctEgwsly9.jpg",
+  },
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/01/63/64/90/1000_F_163649065_AeYO81p8nhDbVOipUwpn7m6gbOnpbMrc.jpg",
+  },
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/03/51/30/60/1000_F_351306077_w7pa3jVVU4fmfiynHr8Pz7ydK4DtlxPY.jpg",
+  },
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/02/96/35/64/1000_F_296356423_f6IEidPVRWzaqj2MJQ2VLJJTYGRAtY4P.jpg",
+  },
+  {
+    "url": "https://as2.ftcdn.net/v2/jpg/02/23/97/75/1000_F_223977507_E7WUczVvxML0nmtW9n70d1qUYHoIBTVF.jpg",
+  },
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/02/23/97/78/1000_F_223977869_cLUHpzKaRP9ulA94EWyhmYa7u2LhXg3z.jpg",
+  },
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/03/14/31/42/1000_F_314314295_bJdRwMklRc9j53iRBHGf2BKBdrdMo87F.jpg",
+  },
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/03/17/06/14/1000_F_317061412_UPBhUHVwgo2nDCUzSZ7wcQA9dzwCH7Tc.jpg",
+  },
+  {
+    "url": "https://as1.ftcdn.net/v2/jpg/01/74/21/08/1000_F_174210895_uiy8rcFJqTKP3owkEilpjTNEy9P3zS4j.jpg",
+  },
+  {
+    "url": "https://as2.ftcdn.net/v2/jpg/01/91/17/31/1000_F_191173191_ZXS5w0PrEDQEFJViNXeAPaqfMTZE5bxV.jpg",
+  },
+]
