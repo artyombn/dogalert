29bf904fbd31dc8a2d084264acda0bd1d8a75e94
artyombn <balabashinan@gmail.com>
2025-04-25 18:56:23 +0300

    add: migrations and filling db

---
+++ run_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#chmod +x run_all.sh
+#./run_all.sh
+
+set -e
+
+echo "Starting run_all.sh"
+
+#echo "Checking installed packages"
+#pip list
+
+echo "Applying migrations..."
+alembic upgrade head
+echo "Migration done"
+
+echo "Filling DB..."
+make filldb_users
+make filldb_pets
+make filldb_reports
+echo "Filling DB done"
+
+echo "Starting FastAPI..."
+uvicorn src.fastapi.main:app --host 0.0.0.0 --port 8001 --reload
