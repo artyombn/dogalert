1071e812974e603ebedb1ca53c43162c76c24e56
artyombn <balabashinan@gmail.com>
2025-04-28 18:37:29 +0300

    fix: send initData to url instead of telegram_id

--- src/fastapi/static/js/tg_initData.js
+++ src/fastapi/static/js/tg_initData.js
@@ -2,21 +2,17 @@ async function checkUser() {
     const tg = window.Telegram.WebApp;
     const initData = tg.initData;
 
-    console.log("Fresh initData:", initData);
-
     const response = await fetch('/check_user', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ initData: initData })
     });
 
     const data = await response.json();
 
     if (data.exists) {
-        window.location.href = '/profile';
+        window.location.href = `/profile?initData=${encodeURIComponent(initData)}`;
     } else {
-        window.location.href = '/agreement';
+        window.location.href = `/agreement?initData=${encodeURIComponent(initData)}`;
     }
 }
