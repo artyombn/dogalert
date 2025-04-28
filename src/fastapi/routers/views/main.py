93376383299f75683f9fb3d44cdde49b0693f348
artyombn <balabashinan@gmail.com>
2025-04-28 18:38:54 +0300

    upd: initData handlers

--- src/fastapi/routers/views/main.py
+++ src/fastapi/routers/views/main.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from src.database.db_session import get_async_session
-from src.fastapi.dependencies.telegram_auth import get_current_user
+from src.fastapi.dependencies.telegram_auth import get_current_user, get_tg_id_from_initdata
 from src.schemas import User as User_schema
 from src.services.report_service import ReportServices
 from src.services.user_service import UserServices
@@ -27,21 +27,32 @@ async def show_users_page(
         "users": users,
     })
 
-@router.get("/profile", response_class=HTMLResponse, include_in_schema=True)
+@router.get("/profile", response_model=None, include_in_schema=True)
 async def show_profile_page(
         request: Request,
         session: AsyncSession = Depends(get_async_session),
+        initData: str | None = None,
 ) -> HTMLResponse:
+    if not initData:
+        return templates.TemplateResponse("no_telegram_login.html", {"request": request})
 
-    user_id = 2
-    user_db = await UserServices.find_one_or_none_by_user_id(user_id, session)
-    pets = await UserServices.get_all_user_pets(user_id, session)
-    reports = await UserServices.get_all_user_reports(user_id, session)
+    telegram_id = get_tg_id_from_initdata(initData)
+    if telegram_id is None:
+        return templates.TemplateResponse("no_telegram_login.html", {"request": request})
+
+    user_db = await UserServices.find_one_or_none_by_tgid(telegram_id, session)
+    if user_db is None:
+        return templates.TemplateResponse("no_telegram_login.html", {"request": request})
+
+    pets = await UserServices.get_all_user_pets(user_db.id, session)
+    reports = await UserServices.get_all_user_reports(user_db.id, session)
+
     return templates.TemplateResponse("profile.html", {
         "request": request,
         "user": user_db,
         "pets": pets,
         "reports": reports,
+        "initData": initData,
     })
 
 @router.get("/reports", response_class=HTMLResponse, include_in_schema=True)
@@ -58,10 +69,37 @@ async def show_reports_page(
 @router.get("/agreement", response_class=HTMLResponse, include_in_schema=True)
 async def show_agreement_page(
         request: Request,
+        initData: str | None = None,
 ) -> HTMLResponse:
-    return templates.TemplateResponse("agreement.html", {
-        "request": request,
-    })
+    if initData:
+        telegram_id = get_tg_id_from_initdata(initData)
+        return templates.TemplateResponse("agreement.html", {
+            "request": request,
+            "telegram_id": telegram_id,
+            "initData": initData,
+        })
+    else:
+        return templates.TemplateResponse("no_telegram_login.html", {
+            "request": request,
+        })
+
+
+@router.get("/registration", response_class=HTMLResponse, include_in_schema=True)
+async def show_registration_page(
+        request: Request,
+        initData: str | None = None,
+) -> HTMLResponse:
+    if initData:
+        telegram_id = get_tg_id_from_initdata(initData)
+        return templates.TemplateResponse("registration.html", {
+            "request": request,
+            "telegram_id": telegram_id,
+            "initData": initData,
+        })
+    else:
+        return templates.TemplateResponse("no_telegram_login.html", {
+            "request": request,
+        })
 
 @router.post("/check_user")
 async def check_user(
