e3150a126b40cfec8652175f689e176c566f58c4
artyombn <balabashinan@gmail.com>
2025-04-30 19:00:41 +0300

    add: run setting including polling & web bots

---
+++ src/app.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+import uvicorn
+
+from src.bot.create_bot import dp, bot
+from src.web.main import app
+
+logger = logging.getLogger(__name__)
+
+async def run_web():
+    config = uvicorn.Config(app, host="0.0.0.0", port=8001, reload=True)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+async def run_bot():
+    await dp.start_polling(bot)
+
+async def main():
+    try:
+        tasks = [
+            run_web(),
+            # run_bot()
+        ]
+        await asyncio.gather(*tasks)
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
