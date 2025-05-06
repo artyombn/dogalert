cbef447b610aed5625a7dff633948b9c96c379d5
artyombn <balabashinan@gmail.com>
2025-05-06 14:01:51 +0300

    add: [aiohttp] get tg url photo by file_id func

---
+++ src/web/dependencies/photo_from_telegram.py
@@ -0,0 +1,29 @@
+import logging
+
+import aiohttp
+
+from src.config.config import settings
+
+logger = logging.getLogger(__name__)
+
+# async def get_file_url_by_file_id(file_id: str) -> str | None:
+#     get_file_url = f"https://api.telegram.org/bot{settings.TOKEN}/getFile?file_id={file_id}"
+#     async with httpx.AsyncClient() as client:
+#         response = await client.get(get_file_url)
+#         if response.status_code != 200 or not response.json().get("ok"):
+#             return None
+#         file_path = response.json()["result"]["file_path"]
+#         return f"https://api.telegram.org/file/bot{settings.TOKEN}/{file_path}"
+
+async def get_file_url_by_file_id(file_id: str, session: aiohttp.ClientSession) -> str | None:
+    get_file_url = f"https://api.telegram.org/bot{settings.TOKEN}/getFile?file_id={file_id}"
+    async with session.get(get_file_url) as response:
+        if response.status != 200:
+            return None
+        data = await response.json()
+        if not data.get("ok"):
+            return None
+        file_path = data["result"]["file_path"]
+        return f"https://api.telegram.org/file/bot{settings.TOKEN}/{file_path}"
+
+
